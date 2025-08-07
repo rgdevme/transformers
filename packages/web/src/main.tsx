563867b0b86554b3946e36bd56e949d65a30231d
Rafael González <reggon81@gmail.com>
2025-08-07 14:42:51 +0200

    feat(web): initial ui

---
+++ packages/web/src/main.tsx
@@ -0,0 +1,21 @@
+import { createTheme, MantineProvider } from '@mantine/core'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+import '@mantine/core/styles.css'
+import './index.css'
+// ‼️ import dropzone styles after core package styles
+import '@mantine/dropzone/styles.css'
+
+const theme = createTheme({
+	/** Put your mantine theme override here */
+})
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<MantineProvider theme={theme}>
+			<App />
+		</MantineProvider>
+	</StrictMode>
+)
