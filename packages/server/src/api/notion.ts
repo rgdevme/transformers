c7b019f7f44e75f04d8ca82053e8a12e6ae55b31
Rafael González <reggon81@gmail.com>
2025-08-10 21:55:52 +0200

    fix(server): update notion calls

--- packages/server/src/api/notion.ts
+++ packages/server/src/api/notion.ts
@@ -1,7 +1,15 @@
-import { Client } from '@notionhq/client'
+import {
+	Client,
+	type CreatePageParameters,
+	type PageObjectResponse
+} from '@notionhq/client'
+import type { NotionRecord } from '../models/notion'
+import { getDatedRates } from './openexchange'
+import { flattenNotionPage } from '../utils/notion'
+import dayjs from 'dayjs'
 
 // Initializing a client
-const notion = new Client({ auth: process.env.NOTION_TOKEN })
+const notion = new Client({ auth: import.meta.env.VITE_NOTION_TOKEN })
 
 const DB_TRANSACTIONS = '23743364f7c780cb90f0f2404505d96f'
 const DB_ACCOUNTS = '23743364f7c780b18369d70251eb81d6'
@@ -11,31 +19,77 @@ const DB_MONTHS = '23743364f7c780ac89bad0f4e513c4c4'
 
 const getDatabasePages = async (database_id: string) => {
 	const database = await notion.databases.query({ database_id })
-	const promises = database.results.map(page =>
-		notion.pages.retrieve({ page_id: page.id })
+	const promises = database.results.map(
+		page =>
+			notion.pages.retrieve({ page_id: page.id }) as Promise<PageObjectResponse>
 	)
 	const pages = await Promise.all(promises)
 	return pages
 }
 
 export const getCategories = async () => {
-	const categories = getDatabasePages(DB_CATEGORIES)
-	return categories
+	const categories = await getDatabasePages(DB_CATEGORIES)
+	return categories.map(c => {
+		const flattened = flattenNotionPage<{
+			id: string
+			url: string
+			Type: string
+			Category: string
+			Keywords: string
+		}>(c)
+		return {
+			...flattened,
+			terms: new RegExp(
+				flattened.Keywords.split(',')
+					.map(kw => kw.trim())
+					.join('|')
+			)
+		}
+	})
 }
 
 export const getAccounts = async () => {
-	const categories = getDatabasePages(DB_ACCOUNTS)
-	return categories
+	const accounts = await getDatabasePages(DB_ACCOUNTS)
+	return accounts.map(
+		flattenNotionPage<{
+			id: string
+			url: string
+			Currency: string
+			Name: string
+		}>
+	)
 }
 
 export const getCurrencies = async () => {
-	const categories = getDatabasePages(DB_CURRENCIES)
-	return categories
+	const currencies = await getDatabasePages(DB_CURRENCIES)
+	return currencies.map(
+		flattenNotionPage<{
+			id: string
+			url: string
+			'HUF rate': number
+			'Last edited time': string // Date
+			Symbol: string
+			Name: string
+			Ticker: string
+		}>
+	)
 }
 
 export const getMonths = async () => {
-	const categories = getDatabasePages(DB_MONTHS)
-	return categories
+	const months = await getDatabasePages(DB_MONTHS)
+	return months.map(c => {
+		const flattened = flattenNotionPage<{
+			id: string
+			url: string
+			'Created time': string // Date
+			Date: string
+			Month: string // Date
+		}>(c)
+		return {
+			...flattened,
+			date: dayjs(flattened.Month, 'YYYY-MM-DD').startOf('month')
+		}
+	})
 }
 
 export const createMonth = async () => {
@@ -58,6 +112,37 @@ export const createMonth = async () => {
 	})
 }
 
-const updateRates = async () => {
-	await notion.pages.update({ page_id: '', properties: {} })
+export const createTransaction = async (tsx: NotionRecord) => {
+	const properties: CreatePageParameters['properties'] = {
+		REF: { rich_text: [{ text: { content: tsx.ref } }] },
+		Transaction: { title: [{ text: { content: tsx.description } }] },
+		Date: { date: { start: tsx.date } },
+		Amount: { number: tsx.amount },
+		'Rate per HUF': { number: tsx.rate }
+	}
+	if (tsx.month) {
+		properties['Month'] = { relation: [{ id: tsx.month }] }
+	}
+	if (tsx.into) {
+		properties['Into'] = { relation: [{ id: tsx.into }] }
+	}
+	if (tsx.from) {
+		properties['From'] = { relation: [{ id: tsx.from }] }
+	}
+	if (tsx.source) {
+		properties['Source'] = { relation: [{ id: tsx.source }] }
+	}
+	if (tsx.category) {
+		properties['Expense Category'] = { relation: [{ id: tsx.category }] }
+	}
+
+	return notion.pages.create({
+		parent: { database_id: DB_TRANSACTIONS, type: 'database_id' },
+		properties
+	})
+}
+
+export const updateCurrency = async (page_id: string, date: string) => {
+	const rateInfo = getDatedRates(date)
+	await notion.pages.update({ page_id, properties: {} })
 }
