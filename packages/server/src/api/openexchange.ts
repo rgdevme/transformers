b1bfce0ac6f4abb8a6e728377be485cfedcb3d94
Rafael González <reggon81@gmail.com>
2025-08-10 21:55:18 +0200

    feat(server): add exchange api

---
+++ packages/server/src/api/openexchange.ts
@@ -0,0 +1,39 @@
+import fs from 'fs'
+import axios from 'axios'
+import path from 'path'
+import type { OpenExchangeRates, RateInfo } from '../types/rates'
+
+const xr = axios.create({ baseURL: 'https://openexchangerates.org/api' })
+
+// Make sure app_id is always included as a param.
+// That way, all our requests are properly authenticated.
+xr.interceptors.request.use(req => {
+	req.params = { ...req.params, app_id: import.meta.env.VITE_OER_TOKEN }
+	return req
+})
+
+export const getDatedRates = async (date: string) => {
+	const filepath = path.resolve(__dirname, '../backups/rates', `${date}.json`)
+
+	if (!fs.existsSync(filepath)) {
+		// To avoid over-consuming our free api we store historical data :D
+		const {
+			data: { rates }
+		} = await xr.get<OpenExchangeRates.Historical>(`/historical/${date}.json`, {
+			params: { symbols: 'USD,EUR,HUF' }
+		})
+
+		const HUF = rates.HUF / rates.HUF
+		const USD = 1 / (rates.USD / rates.HUF)
+		const EUR = 1 / (rates.EUR / rates.HUF)
+
+		const rateInfo: RateInfo = { date, HUF, USD, EUR }
+		fs.writeFileSync(filepath, JSON.stringify(rateInfo), 'utf-8')
+		// Return the info after creating the file,
+		// to avoid reading the file once again
+		return rateInfo
+	}
+
+	var file = fs.readFileSync(filepath, 'utf-8')
+	return JSON.parse(file.toString()) as RateInfo
+}
